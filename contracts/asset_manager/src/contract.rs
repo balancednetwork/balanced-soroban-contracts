e0ba0663ad5d459a955202a598282276231737d2
sagar sapkota <sapkotasagar51@yahoo.com>
2024-06-17 20:22:20 +0545

    code optimization

--- contracts/asset_manager/src/contract.rs
+++ contracts/asset_manager/src/contract.rs
@@ -40,19 +40,19 @@ impl AssetManager {
         get_config(&env)
     }
 
-    pub fn set_admin(e: Env, new_admin: Address) {
-        let admin = read_administrator(&e);
+    pub fn set_admin(env: Env, new_admin: Address) {
+        let admin = read_administrator(&env);
         admin.require_auth();
 
-        write_administrator(&e, &new_admin);
+        write_administrator(&env, &new_admin);
     }
 
-    pub fn get_admin(e: Env) -> Address{
-        read_administrator(&e)
+    pub fn get_admin(env: Env) -> Address{
+        read_administrator(&env)
     }
 
     pub fn configure(env:Env, config: ConfigData){
-        let admin = read_administrator(&env.clone());
+        let admin = read_administrator(&env);
         admin.require_auth();
 
         set_config(&env, config);
@@ -60,60 +60,61 @@ impl AssetManager {
 
     pub fn configure_rate_limit(
         env: Env,
-        token: Address,
+        token_address: Address,
         period: u128,
         percentage: u128,
-    ) {
-        let admin = read_administrator(&env.clone());
+    ) -> Result<(), ContractError> {
+        let admin = read_administrator(&env);
         admin.require_auth();
-        let tokens = read_tokens(&env.clone());
-        if tokens.contains(&token) {
-            panic_with_error!(&env.clone(), ContractError::TokenExists)
+        let tokens = read_tokens(&env);
+        if tokens.contains(&token_address) {
+            return Err(ContractError::TokenExists)
         }else{
-            write_tokens(&env, token.clone());
+            write_tokens(&env, token_address.clone());
         };
 
         if percentage > POINTS {
-            panic_with_error!(&env.clone(), ContractError::PercentageShouldBeLessThanOrEqualToPOINTS); 
+            return Err(ContractError::PercentageShouldBeLessThanOrEqualToPOINTS); 
         }
-        let token_client = token::Client::new(&env, &token);
+        let token_client = token::Client::new(&env, &token_address);
         let contract_token_balance = token_client.balance(&env.current_contract_address());
         
-        write_token_period(&env.clone(), &token.clone(), period);
-        write_token_percentage(&env.clone(), &token.clone(), percentage);
-        write_token_last_update(&env.clone(), &token.clone(), env.ledger().timestamp());
-        write_token_current_limit(&env.clone(), &token.clone(), (contract_token_balance as u128) * percentage/POINTS);
+        write_token_period(&env, &token_address, period);
+        write_token_percentage(&env, &token_address, percentage);
+        write_token_last_update(&env, &token_address, env.ledger().timestamp());
+        write_token_current_limit(&env, &token_address, (contract_token_balance as u128) * percentage/POINTS);
+        Ok(())
     }
 
-    pub fn get_rate_limit(env: Env, token: Address ) -> (u128, u128, u64, u128){
+    pub fn get_rate_limit(env: Env, token_address: Address ) -> (u128, u128, u64, u128){
         (
-            read_token_period(&env, &token.clone()),
-            read_token_percentage(&env, &token.clone()),
-            read_token_last_update(&env, &token.clone()),
-            read_token_last_current_limit(&env, &token.clone()),
+            read_token_period(&env, &token_address),
+            read_token_percentage(&env, &token_address),
+            read_token_last_update(&env, &token_address),
+            read_token_last_current_limit(&env, &token_address),
         )
     }
 
     pub fn reset_limit(env: Env, token: Address){
-        let balance = Self::get_token_balance(env.clone(), token.clone());
-        let percentage: u128 = read_token_percentage(&env, &token.clone());
+        let balance = Self::get_token_balance(&env, token.clone());
+        let percentage: u128 = read_token_percentage(&env, &token);
 
-        env.storage().instance().set(&DataKey::CurrentLimit(token.clone()), &(balance*percentage/POINTS));
+        write_token_current_limit(&env, &token, balance*percentage/POINTS);
     }
 
     pub fn get_withdraw_limit(env: Env, token: Address) -> Result<u128, ContractError>  {
-        let balance = Self::get_token_balance(env.clone(), token.clone());
-        return Ok(Self::calculate_limit(env, balance, token)?)
+        let balance = Self::get_token_balance(&env, token.clone());
+        return Ok(Self::calculate_limit(&env, balance, token)?)
     }
 
-    fn get_token_balance(env: Env, token: Address) -> u128 {
-        let token_client = token::Client::new(&env, &token);
+    fn get_token_balance(env: &Env, token: Address) -> u128 {
+        let token_client = token::Client::new(env, &token);
         return token_client.balance(&env.current_contract_address()) as u128
     }
 
     pub fn verify_withdraw(env: Env, token: Address, amount: u128) -> Result<bool, ContractError> {
-        let balance = Self::get_token_balance(env.clone(), token.clone());
-        let limit = Self::calculate_limit(env.clone(), balance, token.clone())?;
+        let balance = Self::get_token_balance(&env, token.clone());
+        let limit = Self::calculate_limit(&env, balance, token.clone())?;
         if balance - amount < limit { panic_with_error!(&env, ContractError::ExceedsWithdrawLimit); };
 
         write_token_current_limit(&env, &token.clone(), limit);
@@ -121,9 +122,9 @@ impl AssetManager {
         Ok(true)
     }
 
-    pub fn calculate_limit(env: Env, balance: u128, token: Address) -> Result<u128, ContractError> {
-        let period: u128 = read_token_period(&env, &token.clone());
-        let percentage: u128 =  read_token_percentage(&env, &token.clone());
+    pub fn calculate_limit(env: &Env, balance: u128, token: Address) -> Result<u128, ContractError> {
+        let period: u128 = read_token_period(env, &token.clone());
+        let percentage: u128 =  read_token_percentage(env, &token.clone());
         if period == 0 {
             return Ok(0);
         }
@@ -142,7 +143,7 @@ impl AssetManager {
         }
 
         let reserve = if reserve > min_reserve { reserve } else { min_reserve };
-         Ok(reserve)
+        Ok(reserve)
     }
 
     pub fn deposit(
@@ -168,8 +169,8 @@ impl AssetManager {
         data: Bytes,
     ) -> Result<(), ContractError> {
        from.require_auth();
-       let current_address = e.clone().current_contract_address();
-       Self::transfer_token_to(e.clone(), from.clone(), token.clone(), current_address.clone(), amount);
+       let current_address = e.current_contract_address();
+       Self::transfer_token_to(&e, from.clone(), token.clone(), current_address.clone(), amount);
 
        let xcall_message: Deposit = Deposit::new(
             token.to_string(),
@@ -184,29 +185,28 @@ impl AssetManager {
             from.clone(),
             amount
         );
-
-        let rollback_bytes = rollback.encode(&e, String::from_str(&e.clone(), DEPOSIT_REVERT_NAME));
-        let message_bytes = xcall_message.encode(&e, String::from_str(&e.clone(), DEPOSIT_NAME));
-        let (sources, destinations) = Self::xcall_manager(e.clone()).get_protocols();
+        let config = get_config(&e);
+        let rollback_bytes = rollback.encode(&e, String::from_str(&e, DEPOSIT_REVERT_NAME));
+        let message_bytes = xcall_message.encode(&e, String::from_str(&e, DEPOSIT_NAME));
+        let (sources, destinations) = Self::xcall_manager(&e, &config.xcall_manager ).get_protocols();
         let message = AnyMessage::CallMessageWithRollback(CallMessageWithRollback { data: message_bytes, rollback: rollback_bytes });
         let envelope: &Envelope = &Envelope {
             destinations,
             message,
             sources
         };
-        let icon_asset_manager = &get_config(&e).icon_asset_manager;
         
-        Self::xcall_client(e).send_call(&from, &current_address, envelope, icon_asset_manager);
+        Self::xcall_client(&e, &config.xcall).send_call(&from, &current_address, envelope, &config.icon_asset_manager);
         Ok(())
     }
 
-    fn xcall_manager(e: Env) -> XcallManagerClient<'static> {
-       let client = XcallManagerClient::new(&e, &get_config(&e).xcall_manager);
+    fn xcall_manager(e: &Env, xcall_manager: &Address) -> XcallManagerClient<'static> {
+       let client = XcallManagerClient::new(e, xcall_manager);
        return client;
     }
 
-    fn xcall_client(e: Env) -> Client<'static> {
-        return xcall::Client::new(&e, &get_config(&e).xcall);
+    fn xcall_client(e: &Env, xcall: &Address) -> Client<'static> {
+        return xcall::Client::new(e, xcall);
     }
 
     pub fn handle_call_message(
@@ -216,13 +216,14 @@ impl AssetManager {
         data: Bytes,
         protocols: Vec<String>
     )  -> Result<(), ContractError>  {
-        get_config(&e).xcall.require_auth();
+        let config = get_config(&e);
+        config.xcall.require_auth();
         
-        if !Self::xcall_manager(e.clone()).verify_protocols(&protocols) {
-          panic_with_error!(&e, ContractError::ProtocolMismatch);
+        if !Self::xcall_manager(&e, &config.xcall_manager).verify_protocols(&protocols) {
+            return Err(ContractError::ProtocolMismatch);
         };
         let method = Deposit::get_method(&e, data.clone());
-        let icon_asset_manager = get_config(&e).icon_asset_manager;
+        let icon_asset_manager = config.icon_asset_manager;
         let current_contract = e.current_contract_address();
         if method == String::from_str(&e, &WITHDRAW_TO_NAME){
             if from != icon_asset_manager{
@@ -234,22 +235,22 @@ impl AssetManager {
                 return Err(ContractError::InvalidAddress);
             }
             
-            Self::withdraw(e, current_contract, Address::from_string(&message.token_address),  Address::from_string(&message.to), message.amount)?;
+            Self::withdraw(&e, current_contract, Address::from_string(&message.token_address),  Address::from_string(&message.to), message.amount)?;
         } else if method == String::from_str(&e, &DEPOSIT_REVERT_NAME){
-            let xcall_network_address = Self::xcall_client(e.clone()).get_network_address();
+            let xcall_network_address = Self::xcall_client(&e, &config.xcall).get_network_address();
     
             if from !=  xcall_network_address {
                 return Err(ContractError::OnlyCallService)
             };
             let message: DepositRevert = DepositRevert::decode(&e.clone(), data);
-            Self::withdraw(e, current_contract, message.token_address,  message.to, message.amount)?;
+            Self::withdraw(&e, current_contract, message.token_address,  message.to, message.amount)?;
         } else {
             return Err(ContractError::UnknownMessageType);
         }
         Ok(())
     }
 
-    pub fn withdraw(e: Env, from: Address, token: Address, to: Address, amount: u128) -> Result<(), ContractError> {
+    pub fn withdraw(e: &Env, from: Address, token: Address, to: Address, amount: u128) -> Result<(), ContractError> {
         if amount <= 0 {
             return Err(ContractError::AmountIsLessThanMinimumAmount);
         }
@@ -261,8 +262,8 @@ impl AssetManager {
         Ok(())
     }
 
-    fn transfer_token_to(e: Env, from: Address, token: Address, to: Address, amount: u128){
-        let token_client = token::Client::new(&e, &token);
+    fn transfer_token_to(e: &Env, from: Address, token: Address, to: Address, amount: u128){
+        let token_client = token::Client::new(e, &token);
         token_client.transfer(&from, &to, &(amount as i128));
     }
 
